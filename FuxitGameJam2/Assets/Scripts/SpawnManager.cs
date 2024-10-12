5a7fcd42ab5b34a7b17d8f2689ff7c6ba2896399
AnnaRanki <ganra010@edu.xamk.fi>
2024-10-12 19:48:14 +0300

    Spawneri

--- FuxitGameJam2/Assets/Scripts/SpawnManager.cs
+++ FuxitGameJam2/Assets/Scripts/SpawnManager.cs
@@ -9,6 +9,7 @@ public class SpawnManager : MonoBehaviour
     public GameObject background;
     private Vector2 spawnPos;
     private Vector2 spawnKoristeet;
+    private Vector2 spawnEsteet;
     private float toimi = -7;
 
     //Esteet
@@ -32,12 +33,21 @@ public class SpawnManager : MonoBehaviour
             {
             int esteetIndex = Random.Range(0, esteet.Length);
             int koristeetIndex = Random.Range(0, koristeet.Length);
-            spawnPos = new Vector2(player.transform.position.x+40, 0);
-            spawnKoristeet = new Vector2(player.transform.position.x + 35, 0.64f);
+            int randomi1 = Random.Range(15, 30);
+            int randomi2 = Random.Range(30, 50);
+            int randomi3 = Random.Range(51, 60);
+
+            spawnPos = new Vector2(player.transform.position.x+54, -0.05f);
+            spawnEsteet = new Vector2(player.transform.position.x + randomi1, -2);
+            spawnKoristeet = new Vector2(player.transform.position.x + randomi2, -2);
+
             Instantiate(background, spawnPos, background.transform.rotation);
-            Instantiate(esteet[esteetIndex], spawnPos, esteet[esteetIndex].transform.rotation );
-            Instantiate(koristeet[koristeetIndex], spawnPos, koristeet[koristeetIndex].transform.rotation);
-            toimi = toimi + 20;
+            Instantiate(esteet[esteetIndex], spawnEsteet, esteet[esteetIndex].transform.rotation );
+            Instantiate(koristeet[koristeetIndex], spawnKoristeet, koristeet[koristeetIndex].transform.rotation);
+
+            spawnEsteet = new Vector2(player.transform.position.x + randomi3, -2);
+            Instantiate(esteet[esteetIndex], spawnEsteet, esteet[esteetIndex].transform.rotation);
+            toimi = toimi + 50;
             
             }
 
