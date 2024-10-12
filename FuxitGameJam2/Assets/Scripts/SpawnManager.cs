8ef7e600a6d5253b8388909c1813bb8652b888c7
AnnaRanki <ganra010@edu.xamk.fi>
2024-10-12 17:58:37 +0300

    esteita

--- FuxitGameJam2/Assets/Scripts/SpawnManager.cs
+++ FuxitGameJam2/Assets/Scripts/SpawnManager.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    //Tausta
     public GameObject player;
     public GameObject background;
     private Vector2 spawnPos;
+    private Vector2 spawnKoristeet;
     private float toimi = -7;
+
+    //Esteet
+    public GameObject[] esteet;
+
+    //Koristeet
+    public GameObject[] koristeet;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,21 +25,24 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //player ei ehk toimi
-        //while (transform.position.x > -27)
-        //{
+
           
 
             if (transform.position.x > toimi)
             {
-                spawnPos = new Vector2(player.transform.position.x+40, 0);
-                Instantiate(background, spawnPos, background.transform.rotation);
-                toimi = toimi + 20;
-                //continue;
+            int esteetIndex = Random.Range(0, esteet.Length);
+            int koristeetIndex = Random.Range(0, koristeet.Length);
+            spawnPos = new Vector2(player.transform.position.x+40, 0);
+            spawnKoristeet = new Vector2(player.transform.position.x + 35, 0.64f);
+            Instantiate(background, spawnPos, background.transform.rotation);
+            Instantiate(esteet[esteetIndex], spawnPos, esteet[esteetIndex].transform.rotation );
+            Instantiate(koristeet[koristeetIndex], spawnPos, koristeet[koristeetIndex].transform.rotation);
+            toimi = toimi + 20;
+            
             }
 
             
-        //}
+       
     }
     
 }
