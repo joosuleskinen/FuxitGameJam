67d38605a8dc31e94fd1162a7b1a16e157ae6b5b
joosuleskinen <djole012@edu.xamk.fi>
2024-10-12 18:20:17 +0300

    BugFix
    Door open and closed, Player animation left and right and light fix outside + inside

---
+++ FuxitGameJam2/Assets/Scripts/OpenAndCloseDoor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenAndCloseDoor : MonoBehaviour
+{
+
+    Animator door;
+    [SerializeField] GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        door = GetComponent<Animator>();
+        door.Play("Door_Open", 0, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (player.GetComponent<Rigidbody2D>().velocity.x > 0f)
+        {
+            door = GetComponent<Animator>();
+            door.Play("Door_Close", 0, 0.0f);
+            GetComponent<BoxCollider2D>().isTrigger = false;
+        }
+    }
+}
