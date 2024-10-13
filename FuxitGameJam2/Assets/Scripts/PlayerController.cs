abb71f02710d4c5ac5ee11bb07d7558c7c3e6424
AnnaRanki <ganra010@edu.xamk.fi>
2024-10-13 17:00:03 +0300

    VITTUVITTUVIIMEINEN

--- FuxitGameJam2/Assets/Scripts/PlayerController.cs
+++ FuxitGameJam2/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
--- FuxitGameJam2/Assets/Scripts/SpawnManager.cs
+++ FuxitGameJam2/Assets/Scripts/SpawnManager.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Experimental.GraphView.GraphView;
+
 
 public class SpawnManager : MonoBehaviour
 {
