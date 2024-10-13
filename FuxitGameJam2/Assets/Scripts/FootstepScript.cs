22fb8a228b043f855a8ed22cd64ab2ad6f45a917
RudyBD <grube001@edu.xamk.fi>
2024-10-13 14:40:54 +0300

    Sound

---
+++ FuxitGameJam2/Assets/Scripts/FootstepScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepScript : MonoBehaviour
+{
+    public GameObject footstep;
+    // Start is called before the first frame update
+    void Start()
+    {
+        footstep.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            footsteps();
+        }
+        
+        if (Input.GetKeyDown("d"))
+        {
+            footsteps();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StopFootsteps();
+        }
+
+        if (Input.GetKeyUp("a"))
+        {
+            StopFootsteps();
+        }
+
+        if (Input.GetKeyUp("d"))
+        {
+            StopFootsteps();
+        }
+
+    }
+
+    void footsteps()
+    {
+        footstep.SetActive(true);
+    }
+
+    void StopFootsteps()
+    {
+        footstep.SetActive(false);
+    }
+}
