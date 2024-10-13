1df012733260bb1f73d1f22f5b1aeadd579c6646
AnnaRanki <ganra010@edu.xamk.fi>
2024-10-13 09:33:09 +0300

    Monsterikorjattu

--- FuxitGameJam2/Assets/Scripts/EnemyMovement.cs
+++ FuxitGameJam2/Assets/Scripts/EnemyMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemyMovement : MonoBehaviour
 {
@@ -13,17 +14,21 @@ public class EnemyMovement : MonoBehaviour
     // Start is called before the first frame update
     void FixedUpdate()
     {
-        SpeedModifier = MinDistance / 10;
-        DistanceToPlayer = Vector2.Distance(Player.transform.position, transform.position);
+        if (transform.position.x<790)
+        {
+            SpeedModifier = MinDistance / 10;
+            DistanceToPlayer = Vector2.Distance(Player.transform.position, transform.position);
 
-        if (DistanceToPlayer < MinDistance)
-        {
-            transform.Translate(Vector2.right * Time.deltaTime * Speed * SpeedModifier);
+            if (DistanceToPlayer < MinDistance)
+            {
+                transform.Translate(Vector2.right * Time.deltaTime * Speed * SpeedModifier);
+            }
+            else
+            {
+                transform.Translate(Vector2.right * Time.deltaTime * Speed * (DistanceToPlayer / 10));
+            }
         }
-        else
-        {
-            transform.Translate(Vector2.right * Time.deltaTime * Speed * (DistanceToPlayer / 10));
-        }
+        
     }
 
     // Update is called once per frame
